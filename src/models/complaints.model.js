d218ef8afa91e759e02e23ffc0cc345d429cd372
SOURAV MAHATA <mahatasourav35@gmail.com>
2026-02-01 15:17:09 +0530

    complaint registered api done

--- src/models/complaints.model.js
+++ src/models/complaints.model.js
@@ -1,35 +1,35 @@
 import mongoose from "mongoose";
 
-const complaintSchema = new mongoose.Schema({
-  userId : {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required : true,
-  },
-  ward : {
-    type: String,
-    required : true,
-  },
-  landmark : {
-    type: String,
-    required : true,
-  },
-  street : {
-    type: String,
-    required : true,
-  },
-  category : {
-    type: String,
-    required : true,
-  },
-  description : {
-    type: String,
-    required : true,
-  },
-  additionalNotes : {
-    type: String,
-  },
-  complaint_status: {
+const complaintSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    ward: {
+      type: String,
+      required: true,
+    },
+    landmark: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    additionalNotes: {
+      type: String,
+    },
+    complaint_status: {
       type: String,
       enum: ["Registered", "Pending", "Resolved", "Rejected", "Success"],
       default: "Registered",
@@ -44,8 +44,10 @@ const complaintSchema = new mongoose.Schema({
         ref: "Evidence",
       },
     ],
-}, {
-  timestamps: true,
-})
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const Complaint = mongoose.model("Complaint", complaintSchema);
